35c01f6d7323e0a1432bf576a86e69b93c4e255f
adalpo87 <adalpo87@gmail.com>
2021-04-21 17:51:07 +0200

    finita parte js

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    //funzionalità
+	$('.funz').mouseenter(function(){
+		//$('.dropdown-menu').toggleClass('active');
+		$(this).find('.drop-funz').toggleClass('active');
+	});
+    //abbonamenti
+    $('.abb').mouseenter(function(){
+
+        $(this).find('.drop-abb').toggleClass('active');
+    })
+    //chi siamo
+    $('.who').mouseenter(function(){
+        $(this).find('.drop-who').toggleClass('active');
+    })
+    //aiuto
+    $('.help').mouseenter(function(){
+        $(this).find('.drop-help').toggleClass('active');
+    })
+
+});
+
