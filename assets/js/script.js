d06640d18d0cdf13f431581faed380cc2e04f65e
adalpo87 <adalpo87@gmail.com>
2021-04-22 09:52:29 +0200

    aggiunto mouseleave

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,3 @@
-/* $(function(){
-
-    //funzionalità
-	$('.funz').mouseenter(function(){
-		//$('.dropdown-menu').toggleClass('active');
-		$(this).find('.drop-funz').toggleClass('active');
-	});
-
-
-
-
-    //abbonamenti
-    $('.abb').mouseenter(function(){
-
-        $(this).find('.drop-abb').toggleClass('active');
-    })
-    //chi siamo
-    $('.who').mouseenter(function(){
-        $(this).find('.drop-who').toggleClass('active');
-    })
-    //aiuto
-    $('.help').mouseenter(function(){
-        $(this).find('.drop-help').toggleClass('active');
-    })
-
-});
- */
 
 
 $(function(){
@@ -34,10 +7,13 @@ $(function(){
       $('.drop_down.active').toggleClass('active');
       $(this).find('.drop_down').toggleClass('active');
     });
-  
 
     $('.with-dropdown').click(function(){
       $(this).find('.drop_down').toggleClass('active');
+    });
+
+    $('.with-dropdown').mouseleave(function(){
+        $(this).find('.drop_down').toggleClass('active');
     });
   
   
